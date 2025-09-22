320ca489a13180adad52442c85aad134ca2204cc
Diego Reyes <diegorele13@gmail.com>
2025-09-22 13:03:18 -0500

    refactor(scripts): add index file to export all components
    Centralize component exports for better maintainability and cleaner imports

---
+++ src/views/ScriptsView/components/index.ts
@@ -0,0 +1,7 @@
+// Exportar todos los componentes de ScriptsView
+export { default as ScriptsHeader } from './ScriptsHeader.vue'
+export { default as ScriptsStats } from './ScriptsStats.vue'
+export { default as ScriptsFilters } from './ScriptsFilters.vue'
+export { default as ScriptsList } from './ScriptsList.vue'
+export { default as GenerateScriptModal } from './GenerateScriptModal.vue'
+export { default as ScriptViewModal } from './ScriptViewModal.vue'
